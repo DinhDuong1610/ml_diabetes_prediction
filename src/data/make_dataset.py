4c8f778e6aeb7774767fdb3ef7510905821773ab
Dinh Duong <jenny.180820@gmail.com>
2025-08-03 20:38:48 +0700

    feat(data): data processing

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,3 +1,35 @@
 import pandas as pd
+from ydata_profiling import ProfileReport
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 data = pd.read_csv("data/raw/diabetes.csv")
+
+result = data.describe()
+print(result)
+
+info = data.info()
+print(info)
+
+# profile = ProfileReport(data, title="Diabets report", explorative=True)
+# profile.to_file("reports/report.html")
+
+target = "Outcome"
+x = data.drop(target, axis=1)
+y = data[target]
+
+print(y.value_counts())
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+print(x_train.shape)
+print(y_train.shape)
+print(x_test.shape)
+print(y_test.shape)
+
+print(y_train.value_counts())
+print(y_test.value_counts())
+
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
+
