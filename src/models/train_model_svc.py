d172aff3a7f5d565a9da1179260fe58be9b676a5
Dinh Duong <jenny.180820@gmail.com>
2025-08-04 11:32:34 +0700

    refactor(svc): refactor model suport vector classifier by gridsearch

--- src/models/train_model_svc.py
+++ src/models/train_model_svc.py
@@ -2,6 +2,7 @@ import joblib
 import pandas as pd
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
 import os
 
 def main():
@@ -14,10 +15,30 @@ def main():
     x_test = pd.read_csv(f"{processed_data_path}/x_test.csv")
     y_test = pd.read_csv(f"{processed_data_path}/y_test.csv")
 
-    model = SVC(probability=True)
-    model.fit(x_train, y_train)
+    # model = SVC(probability=True)
+    # model.fit(x_train, y_train)
 
-    y_predict = model.predict(x_test)
+    params = {
+        'C': [0.1, 1, 10, 100],
+        'kernel': ['linear', 'rbf'],
+        'gamma': ['scale', 'auto']
+    }
+
+    model = SVC(probability=True, random_state=42)
+
+    grid_search = GridSearchCV(
+        estimator=model,
+        param_grid=params,
+        scoring='recall',
+        cv=5,
+        verbose=2
+    )
+
+    grid_search.fit(x_train, y_train)
+    print(grid_search.best_params_)
+    print(grid_search.best_score_)
+
+    y_predict = grid_search.predict(x_test)
     print(classification_report(y_test, y_predict))
 
     model_path = f"{models_path}/svc_model.joblib"
