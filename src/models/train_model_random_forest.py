a5fe594f5d087108b91a23c076cbd9db88834e06
Dinh Duong <jenny.180820@gmail.com>
2025-08-04 10:57:36 +0700

    feat(random_forest): train & predict model random forest classifier

---
+++ src/models/train_model_random_forest.py
@@ -0,0 +1,28 @@
+import joblib
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+import os
+
+def main():
+    processed_data_path = "data/processed"
+    models_path = "models"
+    os.makedirs(models_path, exist_ok=True)
+
+    x_train = pd.read_csv(f"{processed_data_path}/x_train.csv")
+    y_train = pd.read_csv(f"{processed_data_path}/y_train.csv")
+    x_test = pd.read_csv(f"{processed_data_path}/x_test.csv")
+    y_test = pd.read_csv(f"{processed_data_path}/y_test.csv")
+
+    model = RandomForestClassifier(n_estimators=200, criterion="gini", random_state=42)
+    model.fit(x_train, y_train)
+
+    y_predict = model.predict(x_test)
+    print(classification_report(y_test, y_predict))
+
+    model_path = f"{models_path}/random_forest_model.joblib"
+    joblib.dump(model, model_path)
+
+if __name__ == '__main__':
+    main()
+
