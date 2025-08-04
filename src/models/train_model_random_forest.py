4feea171a13328f5b3053e1ffc1f803e683afcf5
Dinh Duong <jenny.180820@gmail.com>
2025-08-04 11:25:35 +0700

    refactor(random_forest): refactor random forest model by gridsearch

--- src/models/train_model_random_forest.py
+++ src/models/train_model_random_forest.py
@@ -2,6 +2,7 @@ import joblib
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
 import os
 
 def main():
@@ -10,18 +11,34 @@ def main():
     os.makedirs(models_path, exist_ok=True)
 
     x_train = pd.read_csv(f"{processed_data_path}/x_train.csv")
-    y_train = pd.read_csv(f"{processed_data_path}/y_train.csv")
+    y_train = pd.read_csv(f"{processed_data_path}/y_train.csv").values.ravel()
     x_test = pd.read_csv(f"{processed_data_path}/x_test.csv")
-    y_test = pd.read_csv(f"{processed_data_path}/y_test.csv")
+    y_test = pd.read_csv(f"{processed_data_path}/y_test.csv").values.ravel()
 
-    model = RandomForestClassifier(n_estimators=200, criterion="gini", random_state=42)
-    model.fit(x_train, y_train)
+    # model = RandomForestClassifier(n_estimators=200, criterion="gini", random_state=42)
+    # model.fit(x_train, y_train)
 
-    y_predict = model.predict(x_test)
+    params = {
+        'n_estimators': [100, 200, 300, 400],
+        'criterion': ['gini', 'entropy', 'log_loss'],
+        'max_depth': [10, 20, 30, None],
+        'min_samples_split': [2, 5, 10],
+        'min_samples_leaf': [1, 2, 4],
+        'max_features': ['sqrt', 'log2']
+    }
+
+    grid_search = GridSearchCV(estimator=RandomForestClassifier(random_state=100), param_grid=params, scoring="recall", cv=5, verbose=2)
+    grid_search.fit(x_train, y_train)
+
+    print(grid_search.best_estimator_)
+    print(grid_search.best_params_)
+    print(grid_search.best_score_)
+
+    y_predict = grid_search.predict(x_test)
     print(classification_report(y_test, y_predict))
 
     model_path = f"{models_path}/random_forest_model.joblib"
-    joblib.dump(model, model_path)
+    joblib.dump(grid_search, model_path)
 
 if __name__ == '__main__':
     main()
